6b73e5e9c7ac4a78ef623b8569b93e3d56946eb3
Thanasis Polychronakis <thanpolas@gmail.com>
2014-09-07 13:57:07 +0300

    add and integrate chai-as-promised

--- test/lib/tester.lib.js
+++ test/lib/tester.lib.js
@@ -3,8 +3,12 @@
  */
 
 var sinon = require('sinon');
-var sinonChai = require('sinon-chai');
+
 var chai = require('chai');
+var sinonChai = require('sinon-chai');
+var chaiAsPromised = require('chai-as-promised');
+
+chai.use(chaiAsPromised);
 chai.use(sinonChai);
 
 var tester = module.exports = {};
