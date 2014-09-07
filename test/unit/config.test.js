a7167068fa47a79326712bc0570bf015e77977fb
Thanasis Polychronakis <thanpolas@gmail.com>
2014-09-07 22:28:55 +0300

    enrich config tests

--- test/unit/config.test.js
+++ test/unit/config.test.js
@@ -10,7 +10,7 @@ var expect = chai.expect;
 
 var crude = require('../../');
 
-describe.only('Configuration tests', function () {
+describe('Configuration tests', function () {
   beforeEach(function () {
     this.Entity = tester.entity();
     this.crude = crude('/middleware', this.Entity);
@@ -54,6 +54,61 @@ describe.only('Configuration tests', function () {
       });
       it('delete OP', function () {
         expect(this.Entity.prototype.delete).to.have.been.calledWith({_id: 'one'});
+      });
+    });
+
+    describe('Configure Entity methods', function () {
+      beforeEach(function () {
+        this.Entity.prototype.createNew = sinon.stub();
+        this.Entity.prototype.readNew = sinon.stub();
+        this.Entity.prototype.readLimitNew = sinon.stub();
+        this.Entity.prototype.readOneNew = sinon.stub();
+        this.Entity.prototype.updateNew = sinon.stub();
+        this.Entity.prototype.deleteNew = sinon.stub();
+        this.Entity.prototype.countNew = sinon.stub();
+      });
+      beforeEach(function () {
+        return this.crude.config({
+          entityCreate: 'createNew',
+          entityRead: 'readNew',
+          entityReadLimit: 'readLimitNew',
+          entityReadOne: 'readOneNew',
+          entityUpdate: 'updateNew',
+          entityDelete: 'deleteNew',
+          entityCount: 'countNew',
+        });
+      });
+      beforeEach(function () {
+        this.reqres.req.params.id = 'one';
+        return Promise.all([
+          this.crude.create(this.reqres.req, this.reqres.res),
+          this.crude.readList(this.reqres.req, this.reqres.res),
+          this.crude.readOne(this.reqres.req, this.reqres.res),
+          this.crude.update(this.reqres.req, this.reqres.res),
+          this.crude.delete(this.reqres.req, this.reqres.res),
+        ]);
+      });
+      it('should invoke the right method on create OP', function () {
+        expect(this.Entity.prototype.create).to.not.have.been.called;
+        expect(this.Entity.prototype.createNew).to.have.been.called;
+      });
+      it('should invoke the right method on readList OP', function () {
+        expect(this.Entity.prototype.readLimit).to.not.have.been.called;
+        expect(this.Entity.prototype.readLimitNew).to.have.been.called;
+        expect(this.Entity.prototype.count).to.not.have.been.called;
+        expect(this.Entity.prototype.countNew).to.have.been.called;
+      });
+      it('should invoke the right method on update OP', function () {
+        expect(this.Entity.prototype.update).to.not.have.been.called;
+        expect(this.Entity.prototype.updateNew).to.have.been.called;
+      });
+      it('should invoke the right method on readOne OP', function () {
+        expect(this.Entity.prototype.readOne).to.not.have.been.called;
+        expect(this.Entity.prototype.readOneNew).to.have.been.called;
+      });
+      it('should invoke the right method on delete OP', function () {
+        expect(this.Entity.prototype.delete).to.not.have.been.called;
+        expect(this.Entity.prototype.deleteNew).to.have.been.called;
       });
     });
   });
